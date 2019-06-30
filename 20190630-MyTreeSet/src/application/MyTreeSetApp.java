84615eac89e02a87f95dfe86da3c56eafafc92ed
Dennis Chertkov <Dennis@Chertkov.info>
2019-06-30 15:04:40 +0300

    20190630-2

--- 20190630-MyTreeSet/src/application/MyTreeSetApp.java
+++ 20190630-MyTreeSet/src/application/MyTreeSetApp.java
@@ -9,7 +9,7 @@ import model.MyTreeSet;
 
 public class MyTreeSetApp {
 
-	private static final int NUM_NUM = 10;
+	private static final int NUM_NUM = 100_000;
 	static Random gen = new Random();
 	
 	public static void main(String[] args) {
@@ -17,13 +17,19 @@ public class MyTreeSetApp {
 //				new MyTreeSet<Integer>(Comparator.naturalOrder());
 				new MyTreeSet<Integer>();
 		
-		for (int i = 0; i < NUM_NUM; i++)mtsi.add(i);
-		for (Integer i : mtsi) System.out.print(i + " ");
+		for (int i = 0; i < NUM_NUM; i++)mtsi.add(gen.nextInt());
+//		for (Integer i : mtsi) System.out.print(i + " ");
+		System.out.println();
 		System.out.println(mtsi.size());
-		
-		Iterator<Integer> rit = mtsi.recursiveIterator();
-		while(rit.hasNext()) System.out.print(rit.next() + "  ");
+//		
+//		Iterator<Integer> rit = mtsi.recursiveIterator();
+//		while(rit.hasNext()) System.out.print(rit.next() + "  ");
 		System.out.println(mtsi.deep());
+		
+//		System.out.println(mtsi.contains(5));
+//		mtsi.remove(5);
+//		for (Integer i : mtsi) System.out.print(i + " ");
+		
 	}
 
 }
--- 20190630-MyTreeSet/src/model/MyTreeSet.java
+++ 20190630-MyTreeSet/src/model/MyTreeSet.java
@@ -1,19 +1,30 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
 
+/*
+ Conditional for rebalancing tree:
+ 1. size > N min
+ 2. level > (1+coeff) (Log2(size) + 1)
+ 3. balanced == true;
+ */
+
 public class MyTreeSet<T> implements ITreeSet<T> {
-	
+
+	private static final int MIN_BALANCE = 100;
+	private static final double BALANCE_COEFF = 0.2;
 	private Node<T> root;
 	private int size;
 	private Comparator<T> comparator;
-
+	private boolean balanced = true;
+	
 	public MyTreeSet() {
 		super();
-		comparator = (Comparator<T>)Comparator.naturalOrder();
+		comparator = (Comparator<T>) Comparator.naturalOrder();
 	}
-	
+
 	public MyTreeSet(Comparator<T> comparator) {
 		super();
 		this.comparator = comparator;
@@ -21,53 +32,82 @@ public class MyTreeSet<T> implements ITreeSet<T> {
 
 	@Override
 	public boolean add(T data) {
+		int level = 1;
 		if (root == null) {
 			root = new Node<T>(data);
 			size++;
+//			System.out.println("data: " + data + ",\tlevel: " + level);
 			return true;
 		}
-		
+		level = 2;
 		Node<T> current = root;
 		int compareResult;
 		while (true) {
 			compareResult = comparator.compare(current.getData(), data);
-			if (compareResult == 0) return false;
+			if (compareResult == 0)
+				return false;
 			if (compareResult > 0) {
-				if (current.getLeft() == null) break;
+				if (current.getLeft() == null)
+					break;
 				current = current.getLeft();
-			}else {
-				if (current.getRight() == null) break;
+			} else {
+				if (current.getRight() == null)
+					break;
 				current = current.getRight();
 			}
+			level++;
 		}
-		
+//		System.out.println("data: " + data + ",\tlevel: " + level);
+
 		Node<T> node = new Node<T>(data);
 		node.setParent(current);
-		if (compareResult > 0) current.setLeft(node);
-		else current.setRight(node);
+		if (compareResult > 0)
+			current.setLeft(node);
+		else
+			current.setRight(node);
 		size++;
+
+		if (needBalance(level)) {
+			granBalance();
+		}
 		
 		return true;
 	}
 
+	private double log2(int num){
+		int i = 0;
+		for (; (1<<i) < num; i++){}
+		return (double) i;
+	}
+	
+	private boolean needBalance(int limit) {
+		return 	balanced && 
+				( size > MIN_BALANCE ) && 
+				( 1. + BALANCE_COEFF )* log2(limit)+1 < size;
+	}
+	
 	@Override
 	public boolean contains(T data) {
-		return getNodeByData(data) !=null;
+		return getNodeByData(data) != null;
 	}
 
 	private Node<T> getNodeByData(T data) {
-		if (root == null) return null;
-		
+		if (root == null)
+			return null;
+
 		Node<T> current = root;
 		int compareResult;
 		while (true) {
 			compareResult = comparator.compare(current.getData(), data);
-			if (compareResult == 0) return current;
+			if (compareResult == 0)
+				return current;
 			if (compareResult > 0) {
-				if (current.getLeft() == null) return null;
+				if (current.getLeft() == null)
+					return null;
 				current = current.getLeft();
-			}else {
-				if (current.getRight() == null) return null;
+			} else {
+				if (current.getRight() == null)
+					return null;
 				current = current.getRight();
 			}
 		}
@@ -75,19 +115,86 @@ public class MyTreeSet<T> implements ITreeSet<T> {
 
 	@Override
 	public boolean remove(T data) {
-		// TODO Auto-generated method stub
-		return false;
+		Node<T> toBeRemoved = getNodeByData(data);
+
+		if (toBeRemoved == null)
+			return false;
+
+		if (toBeRemoved.getLeft() == null)
+			removeLeftDisable(toBeRemoved);
+		else if (toBeRemoved.getRight() == null)
+			removeRightDisable(toBeRemoved);
+		else {
+			Node<T> victim = toBeRemoved.getRight().getLeftmost();
+			toBeRemoved.setData(victim.getData());
+			removeLeftDisable(victim);
+		}
+		size--;
+		return true;
 	}
 
+	private void removeRightDisable(Node<T> node) {
+		if (node == root) root = root.getLeft();
+		else {
+			Node<T> left = node.getLeft();
+			Node<T> parent = node.getParent();
+			if (node.isLeftChild()) parent.setLeft(left);
+			else parent.setRight(left);
+
+			if (left != null) left.setParent(parent);
+		}
+	}
+
+	private void removeLeftDisable(Node<T> node) {
+		if (node==root) root = root.getRight();
+		else {
+			Node<T> right = node.getRight();
+			Node<T> parent = node.getParent();
+			if (node.isLeftChild()) parent.setLeft(right);
+			else parent.setRight(right);
+			
+			if (right != null) right.setParent(parent);
+		}
+	}
+
+	private void granBalance() {
+		ArrayList<T> alt = new ArrayList<>();
+		for (T t : this) alt.add(t);
+		root = null;								// clear tree
+		size = 0;									// tree's size = zero
+		unbalanced();
+		putSubArray(alt, 0, alt.size()-1);
+		balanced();
+	}
+	
+	private void putSubArray(ArrayList<T> array, int min, int max) {
+		
+		if (max-min < 2) {
+			add(array.get(min));
+			if (max!= min) add(array.get(max));
+		} else {
+			int middle = min + (max - min)/2;
+			add(array.get(middle));
+			putSubArray(array, min, middle-1);
+			putSubArray(array, middle+1, max);
+		}
+	}
+	
+	public void balanced() {
+		balanced = true;
+	}
+
+	public void unbalanced() {
+		balanced = false;
+	}
+	
 	@Override
 	public int deep() {
 		return subTreeDeep(root);
 	}
 
 	private int subTreeDeep(Node<T> node) {
-		return node == null ? 0 :
-			Integer.max(subTreeDeep(node.getLeft()), 
-				           subTreeDeep(node.getRight())) +1;
+		return node == null ? 0 : Integer.max(subTreeDeep(node.getLeft()), subTreeDeep(node.getRight())) + 1;
 	}
 
 	@Override
@@ -98,11 +205,11 @@ public class MyTreeSet<T> implements ITreeSet<T> {
 	public Iterator<T> iterator() {
 		return new MyTreeSetIteratorI<>(root);
 	}
-	
+
 	public Iterator<T> rangeIterator(T min, T max) {
 		return new MyTreeSetRangeIteratorI<>(root, min, max, comparator);
 	}
-	
+
 	public Iterator<T> recursiveIterator() {
 		return new MyTreeSetIteratorR<>(root);
 	}
